f33492f14a0e18a5fbaf9c608aa9c0734acff4a1
DarminderA <139761188+DarminderA@users.noreply.github.com>
2024-01-29 09:06:12 +0000

    Title effects

--- testapp/pages/index.tsx
+++ testapp/pages/index.tsx
@@ -8,12 +8,31 @@ export const Title = styled.h1`
   color: papayawhip;
 `;
 
+export const Button = styled.button`
+ cursor: pointer;
+ padding: 15px 22px;
+ border-radius: 24px;
+ border: none;
+ font-weight: 500;
+ color: #fff;
+ background: #0096c7;
+ ${(props) =>
+  props.secondary &&
+  `
+  background: #f00;
+  color: #00a8; 
+  border-radius: 0
+  `};
+`;
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <Title>Welcome!</Title>
       </div>
+      <Button>Primary</Button>
+      <Button secondary>Secondary</Button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
